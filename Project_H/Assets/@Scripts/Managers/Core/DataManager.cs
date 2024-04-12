6f54fde234563a2f324f52ca96cb19c5d10811a1
seonghoon_kim <hooony1324@gmail.com>
2024-04-12 23:23:42 +0900

    Managers, Addressable, LoadingScene세팅

---
+++ Project_H/Assets/@Scripts/Managers/Core/DataManager.cs
@@ -0,0 +1,24 @@
+using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ILoader<Key, Value>
+{
+    Dictionary<Key, Value> MakeDict();
+    //bool Validate();
+}
+
+public class DataManager
+{
+    public void Init()
+    {
+
+    }
+
+    private Loader LoadJson<Loader, Key, Value>(string path) where Loader : ILoader<Key, Value>
+    {
+        TextAsset textAsset = Managers.Resource.Load<TextAsset>(path);
+        return JsonConvert.DeserializeObject<Loader>(textAsset.text);
+    }
+}
